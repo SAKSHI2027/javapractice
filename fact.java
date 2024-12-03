e8c11500e33c18fde49a35e12662eaf270517203
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-04 00:55:43 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -1,15 +1,15 @@
-// public class fact {
-//     public static void main(String[] args) {
-//         int n = 5;       // Declare n
-//         int fact = 1;    // Declare and initialize fact
+public class fact {
+    public static void main(String[] args) {
+        int n = 5;       // Declare n
+        int fact = 1;    // Declare and initialize fact
 
-//         for (int i = 1; i <= n; i++) {  
-//             fact = fact * i;  // Multiply fact by i
-//         }
+        for (int i = 1; i <= n; i++) {  
+            fact = fact * i;  // Multiply fact by i
+        }
 
-//         System.out.println("Factorial of " + n + " is: " + fact);  // Print the factorial value
-//     }
+        System.out.println("Factorial of " + n + " is: " + fact);  // Print the factorial value
+    }
 
     
-// }
+}
 
