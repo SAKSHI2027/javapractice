e28e15c5475778542b675918d3d045d72992614c
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-18 01:17:28 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
  public class prime {
     public static void main (String [] arg ){
-        int n, count = 0;
-        System.out.print("Enter the number");
-        Scanner sc = new Scanner(System.in);
+        int n, count = 0;                          // declear the int n and count veriable
+        System.out.print("Enter the number");      // user  input
+        Scanner sc = new Scanner(System.in);       // scan input
         n = sc.nextInt();
-        for (int i =1; i <= n; i++){
-            if ( n % i == 0){
-                count ++;
+        for (int i =1; i <= n; i++){               // run the loop from 1 to n 
+            if ( n % i == 0){                      // check condition n % is 0 if yes then cout ++ for next number 
+                count ++;                          // prime number is divide only with self and 1 
             }
         }
-            if (count == 2)
+            if (count == 2)                          // the count is 2 then its prime number
                System.out.println("prime ");
                else
-               System.out.println("not prime");
+               System.out.println("not prime");    
 
     }
     
