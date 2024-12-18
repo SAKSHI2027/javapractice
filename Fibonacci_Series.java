8a29527022ab8e2bac1968efa71a37d531cfce63
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-18 22:00:39 +0530

    Update Fibonacci_Series.java

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Fibonacci_Series {
     public static void main(String[] args) {
-        int term, a = 0, b = 1, c;
-        System.out.print("Enter the turm");
-        Scanner sc = new Scanner(System.in);
+        int term, a = 0, b = 1, c;                    // decler the number of term and a value b value and c is temporary veriable
+        System.out.print("Enter the turm");           // user input
+        Scanner sc = new Scanner(System.in);          //
         term = sc.nextInt();
-        for (int i = 1; i <= term; i++) {
-            System.out.print(a);
-            c = a + b;
-            a = b;
-            b = c;
+        for (int i = 1; i <= term; i++) {           // run the loop untill i<= term
+            System.out.print(a);                    // print the 1st value
+            c = a + b;                              //c become 0+1= 1
+            a = b;                                  // a become b   :. 1
+            b = c;                                  // b become c    :. 1
         }
 
     }
