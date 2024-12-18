8196ba0aed8d5e900ace9cfc357420347431c63b
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-18 21:54:54 +0530

    Add files via upload

---
+++ Fibonacci_Series.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        int term, a = 0, b = 1, c;
+        System.out.print("Enter the turm");
+        Scanner sc = new Scanner(System.in);
+        term = sc.nextInt();
+        for (int i = 1; i <= term; i++) {
+            System.out.print(a);
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+    }
+
+}
