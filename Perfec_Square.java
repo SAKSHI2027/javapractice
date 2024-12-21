2230d9008712e2c35cdb5a124b8e7d7df0e4b00a
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-21 13:31:07 +0530

    Add files via upload

---
+++ Perfec_Square.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Perfec_Square {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+        if (n > 0)
+          {
+           int squareRoot = (int)Math.sqrt(n);
+           if (squareRoot * squareRoot == n){
+            System.out.print("The number " +  n  + " is perfect square ");
+           }else {
+            System.out.print("the number " +  n  + " is not perfect Square");
+         }
+        } else {
+        System.out.print("the number " +  n  + " is perfect Square");
+
+    }
+}
+    
+}
