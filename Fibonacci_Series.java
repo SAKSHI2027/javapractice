8d7b69820d5765ec961bddc265d1039f84a6015d
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-21 12:27:12 +0530

    Update Fibonacci_Series.java

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class Fibonacci_Series {
     public static void main(String[] args) {
-        int term, a = 0, b = 1, c;
+        int term, a = 0, b = 1, c;               // initialyse the a=0;b=0, and c
         System.out.print("Enter the turm");
         Scanner sc = new Scanner(System.in);
         term = sc.nextInt();
-        for (int i = 1; i <= term; i++) {
-            System.out.print(a);
-            c = a + b;
+        for (int i = 1; i <= term; i++) {       // starting from index 1 
+            
+            System.out.print(a);             ////print  a 
+            c = a + b;                       
             a = b;
             b = c;
         }
