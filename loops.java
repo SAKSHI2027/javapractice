cf8516dab09ad747c12718c4489e3c931b9f5003
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-30 22:46:00 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -1,40 +1,40 @@
 ///1.... for loop 
-// public class loops {
-//     public static void main (String args[]){
-//         ///counter ++ => counter + 1
-//       for ( int counter = 0; counter < 5; counter = counter ++ )  {
-//         System.out.println("Hello Wold!");
-//       }
-//     }
+public class loops {
+    public static void main (String args[]){
+        ///counter ++ => counter + 1
+      for ( int counter = 0; counter < 5; counter = counter ++ )  {
+        System.out.println("Hello Wold!");
+      }
+    }
     
-// }
+}
  
 
 // q1 print the number from 1 to 10 
 
-// public class loops {
-//     public static void main (String args[]){
-//         ///counter ++ => counter + 1
-//       for ( int i = 0; i < 11; i++ )  {
-//         System.out.println(i);
-//       }
-//     }
+public class loops {
+    public static void main (String args[]){
+        ///counter ++ => counter + 1
+      for ( int i = 0; i < 11; i++ )  {
+        System.out.println(i);
+      }
+    }
     
-// }
+}
 
 
 // 2 ....while loop
-// public class loops {
-//     public static void main (String args[]){
-//         ///counter ++ => counter + 1
-//       int i = 0;
-//       while (i <=10){
-//         System.out.println(i++);
+public class loops {
+    public static void main (String args[]){
+        ///counter ++ => counter + 1
+      int i = 0;
+      while (i <=10){
+        System.out.println(i++);
         
-//       }
-//     }
+      }
+    }
     
-// }
+}
 
 // 3 ...do while loop 
 
